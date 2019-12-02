3c8edcd917bf532719a7b45d5ef6f9daf4470fb9
kalaiselvan <kalai6095@gmail.com>
2019-12-03 00:34:43 +0530

    nav added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarModule,SidebarNavItemModule,SidebarNavModule,SideBarBodyModule } from '@momentum-ui/angular';
+import { TopbarComponent } from './nav/topbar/topbar.component';
+import { MenubarComponent } from './nav/menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SideBarModule.forRoot(),
+    SidebarNavItemModule,SidebarNavModule,SideBarBodyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
